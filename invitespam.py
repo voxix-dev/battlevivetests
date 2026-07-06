9dd56b6ace4a44528af3e750a7e5b9ec56971674
voxix <lukasztobiasiewicz@proton.me>
2026-07-06 18:51:31 +0200

    fixed request, cf_clearance wasn't needed, added while true for mor spam clenup

--- invitespam.py
+++ invitespam.py
@@ -27,18 +27,6 @@ def get_access_token() -> str:
                 token = auth_data["access_token"]
     return token
 
-def get_cf_clearance() -> str:
-    AUTH_FILE = "playwright/.auth/state.json"
-    if not os.path.exists(AUTH_FILE):
-        print(f"Missing: {AUTH_FILE}. Run save_state.py first.")
-        sys.exit(1)
-    with open(AUTH_FILE, "r") as f:
-        state = json.load(f)
-    for cookie in state.get("cookies", []):
-        if cookie["name"] == "cf_clearance" and cookie["domain"] == ".battlevive.com":
-            cf_clearance = cookie["value"] 
-    return cf_clearance
-
 def select_request(headers, url, select="*", filters=None, order=None, limit=None):
     params = {"select": select}
     if filters:
@@ -49,25 +37,20 @@ def select_request(headers, url, select="*", filters=None, order=None, limit=Non
         params["limit"] = str(limit)
     return requests.get(url, headers=headers, params=params)
 
-def invite_player(targetUserId,lobbyId,token,cf_clearance): 
+def invite_player(targetUserId,lobbyId,token): 
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
-        "Cookie":f"cf_clearance={cf_clearance}",
-        "Host":BATTLEVIVE_URL,
-        "Origin":f"https://{BATTLEVIVE_URL}",
     }
-    data = {"lobbyId":lobbyId,"targetUserId":targetUserId}
-    print(f"Request header:{headers}")
-    print(f"Request body:{data}")
-    return requests.post(f"https://{BATTLEVIVE_URL}/api/matchmaking/invite-player",headers=headers,data=data) 
+    json = {"lobbyId":lobbyId,"targetUserId":targetUserId}
+    print(f"Request body: {json}")
+    return requests.post(f"https://{BATTLEVIVE_URL}/api/matchmaking/invite-player",headers=headers,json=json) 
 
 def main():
     if not SUPABASE_URL or not SUPABASE_API_KEY:
         print("Error: hardcode SUPABASE_URL, SUPABASE_API_KEY, before running.", file=sys.stderr)
         sys.exit(1)
     token = get_access_token()
-    cf_clearance = get_cf_clearance()
 
     url = f"{SUPABASE_URL}/rest/v1/lobbies"
     headers = {
@@ -77,25 +60,20 @@ def main():
     }
 
     response = select_request(headers, url, select=id) 
-
+    
     print(f"Lobby Status: {response.status_code}")
-    #print(response.json())
 
     lobbies = response.json()
-
     url = f"{SUPABASE_URL}/rest/v1/users"
     response = select_request(headers, url, select=id)
     print(f"Users Status: {response.status_code}")
 
     users = response.json()
-    for lobby in lobbies:
-        for user in users:
-            response=invite_player(lobbyId=lobby['id'], targetUserId=user['id'],token=token, cf_clearance=cf_clearance)
-            print(f"Invite Status: {response.status_code}")
-            #print(f"Invite Header: {response.headers}")
-            #print(f"Invite Response:{response.json()}")
+    while True :
+        for lobby in lobbies:
+            for user in users:
+                response=invite_player(lobbyId=lobby['id'], targetUserId=user['id'],token=token)
+                print(f"Invite Status: {response.status_code}")
 
-    
-   # print(users)
 if __name__ == "__main__":
     main()
